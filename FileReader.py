3c4d7e9765ec3f87e7480a33c7842b3276bce401
BassamMattar <37397559+BassamMattar@users.noreply.github.com>
2018-05-11 22:52:30 +0200

    interval Type

--- FileReader.py
+++ FileReader.py
@@ -3,8 +3,9 @@ class MyClass(object):
     method = 0
     eqn = 0
     tolerance = 0
-    interval = "????"#TO DO HERE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    interval = []#TO DO HERE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     maxIteration = 0
+    validMethod = True
     def __init__(self, path = ""):
         self.file = open(path, "r")
         
@@ -12,10 +13,31 @@ class MyClass(object):
         lines = self.file.readlines()
 
         self.method = int(lines[0])
+        
         self.eqn = lines[1]
+        
+        if self.method in (1,2,5):
+            self.validMethod = True
+            inter = lines[2]
+            inter = inter.replace("[","")
+            inter = inter.replace("]","")
+            inter = Points.split(" ")
+            for i in inter:
+                self.interval.append(float(i))
+        elif self.method in (3,4,6):
+            self.validMethod = True
+            inter = lines[2]
+            inter = inter.replace("[","")
+            inter = inter.replace("]","")
+            self.interval.append(float(inter))
+        elif self.method == 7:
+            self.validMethod = True
+        else:
+            self.validMethod = False
+            
         self.interval = lines[2]
         self.tolerance = float(lines[3])
         self.maxIteration = int(lines[4])
 
         
-        return self.method,self.eqn,self.interval,self.tolerance,self.maxIteration
+        return self.method,self.eqn,self.interval,self.tolerance,self.maxIteration,self.validMethod
